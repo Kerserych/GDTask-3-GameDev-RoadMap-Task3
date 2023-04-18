132d394366b9515b5e70a8d26c7de3d8cc08684a
Nikita Tsygulev <hekitos007@mail.ru>
2023-04-18 10:30:39 +0300

    Сделал задание

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.WriteLine("Введите A B и С");
+string[] mas = Console.ReadLine().Split(' ');
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i].Contains('.') == true)
+    {
+        mas[i] = mas[i].Replace('.', ',');
+    }
+}
+double res = heron(Convert.ToDouble(mas[0]), Convert.ToDouble(mas[1]), Convert.ToDouble(mas[2]));
+Console.WriteLine(res);
+
+
+double heron(double a, double b, double c)
+{
+    double s = (a + b + c) / 2;
+    return Math.Round((Math.Sqrt(s * (s - a) * (s - b) * (s - c))), 2);
+}
